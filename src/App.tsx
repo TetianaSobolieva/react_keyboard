36cb59c168df5e5be0aeb7040677931957e79d4d
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2026-01-22 10:15:23 +0200

    changed PressKey on PressedKey

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ type State = {
 
 export class App extends React.Component<{}, State> {
   state: State = {
-    pressKey: null,
+    pressedKey: null,
   };
 
   handleKeyboardEvent = (event: KeyboardEvent) => {
-    this.setState({ pressKey: event.key });
+    this.setState({ pressedKey: event.key });
   };
 
   componentDidMount(): void {
@@ -25,8 +25,8 @@ export class App extends React.Component<{}, State> {
     return (
       <div className="App">
         <p className="App__message">
-          {this.state.pressKey
-            ? `The last pressed key is [${this.state.pressKey}]`
+          {this.state.pressedKey
+            ? `The last pressed key is [${this.state.pressedKey}]`
             : 'Nothing was pressed yet'}
         </p>
       </div>
